54338415262453aea47c61d9c23bfade87cb163e
Paulo Silva <engelik@gmail.com>
2016-10-20 15:16:27 +0100

    added jinja templates and template rendering

--- server.py
+++ server.py
@@ -1,50 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+#   Sample data:
+# Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+# Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'id':'1'}
 
 
 # restaurants routes
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-    return 'This will show all the restaurants'
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurant/new')
 def newRestaurant():
-    return 'This will be for making a new restaurant'
+    return render_template('createrestaurant.html')
 
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return 'This will be for editing restaurant %s' % restaurant_id
+    return render_template('editrestaurant.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return 'This will be for deleting restaurant %s' % restaurant_id
+    return render_template('deleterestaurant.html', restaurant=restaurant)
 
 
 # menu item routes
 @app.route('/restaurant/<int:restaurant_id>/menu')
 @app.route('/restaurant/<int:restaurant_id>')
 def showMenu(restaurant_id):
-    return 'This will show the menu for restaurant %s' % restaurant_id
+    return render_template('restaurantmenu.html', restaurant=restaurant, items=items)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return 'This will be for making a new menu'
+    return render_template('createmenuitem.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return 'This will be for editing menu %s' % menu_id
+    return render_template('editmenuitem.html', restaurant=restaurant, item=item)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-    return 'This will be for deleting menu %s' % menu_id
+    return render_template('deletemenuitem.html', restaurant=restaurant, item=item)
 
 if __name__ == '__main__':
     app.debug = True
